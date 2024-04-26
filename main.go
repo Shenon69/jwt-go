75a630289b485ae842b26532a34123afd260f90f
Shenon69 <ask.tikki69@gmail.com>
2024-04-27 02:15:35 +0530

    feat: Initial router

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Shenon69/jwt-go/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnv()
+}
 
 func main() {
 	fmt.Println("World Hello!")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
